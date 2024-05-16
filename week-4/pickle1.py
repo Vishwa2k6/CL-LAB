9d38ae237f7b34749df320841481dd42729ebf67
Vishwa2k6 <159224609+Vishwa2k6@users.noreply.github.com>
2024-05-16 18:47:11 +0530

    Create pickle1.py

---
+++ week-4/pickle1.py
@@ -0,0 +1,19 @@
+#pickle module is used to r/w from binary file
+#pickle.dump(DATA,FILE_handle)-> write datastructure
+#pickle.load(file_handle)->read
+
+import numpy as np
+import pickle
+t=np.arange(0,1,0.01)
+f=5
+x=np.sin(2*np.pi*f*t)
+#write data into file by pickle.dump()
+fil=open("sin_pickle.txt","wb")
+pickle.dump(x,fil)
+fil.close()
+
+#read data from file by pickle.load()
+fil=open("sin_pickle.txt","rb")
+n=pickle.load(fil)
+print(n)
+fil.close()
